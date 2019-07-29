84b0c9b7b7e5ea310d1c40196043c7061a5d7f2f
Jingi Kim <copyx00@gmail.com>
2019-07-29 01:34:32 +0000

    Merged in graceful_exit_for_db (pull request #1)
    feat: 프로세스 종료 시 DB 연결 해제하는 로직 추가
    
    https://trello.com/c/FVlxoLrs
    
    Approved-by: Yena Kim <yenarue@gmail.com>

--- db.js
+++ db.js
@@ -1,17 +1,51 @@
 const mongoose = require('mongoose');
 const config = require('./config');
 
+const TAG = '[DB]';
 const connectionMap = {};
 
-const add = (connectionName, dbUrl) => {
+(function init() {
+    const gracefulExit = cause => {
+        const connectionClosePromises = Object.keys(connectionMap)
+            .map(key => connectionMap[key].close()
+                .catch(err => `Error occured on closing ${key}:\n${err}`));
 
+        Promise.all(connectionClosePromises)
+            .then(results => {
+                results.forEach(result => {
+                    if (result) {
+                        console.error(result)
+                    }
+                });
+                console.log(TAG, `Connections are disconnected due to exit process by ${cause}`);
+                process.exit();
+            })
+            .catch(err => {
+                console.error(TAG, `Error occured on disconnecting due to exit process by ${cause}:\n`, err);
+                process.exit();
+            });
+    };
+
+    ['exit', 'SIGINT', 'SIGTERM', 'SIGQUIT'].forEach(signal => {
+        process.on(signal, gracefulExit);
+    });
+
+    process.on('uncaughtException', err => {
+        console.error('Uncaught Exception:\n', err);
+        gracefulExit('uncaught exception');
+    });
+})();
+
+const add = (connectionName, dbUrl) => {
     let connectionInfo = {
+        name: connectionName,
         url: dbUrl,
         connection: null
     };
 
     Object.defineProperty(connectionMap, connectionName, {
-        configurable: false,
+        configurable: true,
+        enumerable: true,
         get: () => {
             return getConnection(connectionInfo);
         },
@@ -21,25 +55,30 @@ const add = (connectionName, dbUrl) => {
 
 const getConnection = (connectionInfo) => {
     if (!connectionInfo.connection || connectionInfo.connection.readyState === 0) {
-        connectionInfo.connection = mongoose.createConnection(connectionInfo.url);
+        connectionInfo.connection = mongoose.createConnection();
 
-        setRecoverConfig(connectionInfo);
+        connectionInfo.connection.on('connected', () => {
+            console.log(TAG, `${connectionInfo.name} is connected`);
+        });
+        connectionInfo.connection.on('error', err => {
+            console.error(TAG, `${connectionInfo.name} occurred error:\n`, err);
+        });
+        connectionInfo.connection.on('disconnected', () => {
+            console.log(TAG, `${connectionInfo.name} is disconnected`);
+            delete connectionMap[connectionInfo.name];
+        });
+
+        connectionInfo.connection.openUri(connectionInfo.url, {useNewUrlParser: true})
+            .catch(err => {
+                console.error(TAG, `Error is occurred on connecting to ${connectionInfo.name}:\n`, err);
+            });
     }
 
     return connectionInfo.connection;
 };
 
-const setRecoverConfig = (connectionInfo) => {
-    const retryConnect = () => {
-        getConnection(connectionInfo);
-    };
-
-    connectionInfo.connection.on('disconnected', retryConnect);
-};
-
 // 새 DB를 연결하고 싶다면, 아래에 add 함수로 호출하세요.
 // call `add` function if you want to connect new db.
 add('FOMES', config.fomesDbUrl);
-add('ADMIN_AGENDA', config.agendaDbUrl);
 
 module.exports = connectionMap;
