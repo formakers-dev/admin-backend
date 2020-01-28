2710761681e1af0825b1449b063272d9be756ea6
yenarue <yenarue@gmail.com>
2020-01-28 22:14:20 +0900

    chore: index 페이지에 버전 정보 추가
    https://trello.com/c/dawA7Hwd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+
+const packagejson = require('../package.json');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fomes Admin Server' });
+  res.render('index', { title: 'Fomes Admin Server (' + packagejson.version + ')' });
 });
 
 module.exports = router;
