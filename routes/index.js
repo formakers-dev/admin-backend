a7a24794ca874c06437f3ab8bba3def8b09e7aa6
yenarue <yenarue@gmail.com>
2020-01-28 22:23:09 +0900

    feat: index 화면에 릴리즈 레벨 추가

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fomes Admin Server (' + packagejson.version + ')' });
+  res.render('index', { title: 'Fomes Admin Server - ' + process.env.NODE_ENV + ' (' + packagejson.version + ')' });
 });
 
 module.exports = router;
