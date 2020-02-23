2975d37bb0d0c07623dfd3f2584932db1806e6bd
formakers.dev <formakers.dev@gmail.com>
2020-02-23 18:23:10 +0900

    feat : 노티 수정 API 추가
    예약된 노티 수정하기: https://trello.com/c/IUXbRWPY

--- routes/noti.js
+++ routes/noti.js
@@ -4,8 +4,11 @@ const Controller = require('../controllers/noti');
 
 /* GET users listing. */
 router.post('/', Controller.sendNoti);
+router.put('/', Controller.updateReservedNoti);
+
 router.post('/topics/:topic', Controller.sendNotiByTopic);
+router.put('/topics/:topic', Controller.updateReservedNotiByTopic);
+
 router.post('/reserved/cancel', Controller.cancelReservedNoti);
 router.get('/reserved', Controller.getReservedNotiList);
-
 module.exports = router;
