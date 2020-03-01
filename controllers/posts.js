129709353196c11e0fcaa6f1f056ff084b28abdd
formakers.dev <formakers.dev@gmail.com>
2020-03-01 13:40:40 +0900

    feat : Posts 수정/삭제 API 추가
    수정하기: https://trello.com/c/d9V9Z8bW
    삭제하기: https://trello.com/c/c6SdeAaC

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,8 +8,25 @@ const getPosts = (req, res) => {
 
 const registerPost = (req, res) => {
     PostsService.insertPost(req.body)
-        .then(result => res.json(result))
+        .then(result => res.sendStatus(200))
         .catch(err => res.status(500).json({error: err.message}));
 };
 
-module.exports = { getPosts, registerPost };
+const updatePost = (req, res) => {
+    PostsService.updatePost(req)
+        .then(result => res.sendStatus(200))
+        .catch(err => res.status(500).json({error: err.message}));
+};
+
+const deletePost = (req, res) => {
+    PostsService.deletePost(req)
+        .then(result => res.sendStatus(200))
+        .catch(err => res.status(500).json({error: err.message}));
+};
+
+const deletePosts = (req, res) => {
+    PostsService.deletePosts(req)
+        .then(result => res.sendStatus(200))
+        .catch(err => res.status(500).json({error: err.message}));
+};
+module.exports = { getPosts, registerPost, updatePost, deletePost, deletePosts };
--- services/posts.js
+++ services/posts.js
@@ -10,12 +10,33 @@ const findPublishablePosts = () => {
 };
 
 const insertPost = (post) => {
-    console.log(post);
+    console.log('insertPost');
     return new Posts(post).save();
+};
+
+const updatePost = (req) => {
+    console.log('updatePost');
+    console.log(req.body);
+    return Posts.replaceOne({_id: req.params.id}, req.body);
+};
+
+const deletePost = (req) => {
+    console.log('deletePost');
+    console.log(req.params.id);
+    return Posts.deleteOne({_id: req.params.id});
+};
+
+const deletePosts = (req) => {
+    console.log('deletePosts');
+    const objectIds = req.body.map(id => mongoose.Types.ObjectId(id));
+    return Posts.deleteMany({_id: { $in: objectIds}});
 };
 
 module.exports = {
     findPublishablePosts,
-    insertPost
+    insertPost,
+    updatePost,
+    deletePost,
+    deletePosts
 };
 
