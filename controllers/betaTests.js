d9c8269e4688bcbca2d783229e60507471ca7219
formakers.dev <formakers.dev@gmail.com>
2020-05-17 23:09:13 +0900

    feat : 테스트 등록/수정/상세보기/리스트 API 수정 및 개발
    https://trello.com/c/SRuXgcr2

--- controllers/betaTests.js
+++ controllers/betaTests.js
@@ -27,7 +27,34 @@ const getAllBetaTests = (req, res) => {
         });
 };
 
+const updateBetaTest = (req, res) => {
+    console.log('updateBetaTest');
+    const betaTest = req.body;
+    BetaTestService.updateBetaTest(betaTest)
+        .then(result => {
+            console.log(result);
+            res.json(result)
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: err.message});
+        });
+};
+
+const getBetaTest = (req, res) => {
+    BetaTestService.findBetaTest(req.params.id)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: err.message});
+        });
+};
+
 module.exports = {
     registerBetaTest,
-    getAllBetaTests
+    updateBetaTest,
+    getAllBetaTests,
+    getBetaTest
 };
