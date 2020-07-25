cfd53b335e712d83a7ed65848f2eaae3de9d331c
Jason <jason.ryu@formakers.net>
2020-07-25 10:57:14 +0900

    feat: 지표 엑셀에서 활용 위한 betaTest 상세 조회 API 외부 요청 허용
    https://trello.com/c/RL12I8Us

--- routes/betaTests.js
+++ routes/betaTests.js
@@ -6,8 +6,10 @@ const Auth = require('../middlewares/auth');
 
 router.post('/', Auth.verifyToken, Controller.registerBetaTest);
 router.get('/', Auth.verifyToken, Controller.getAllBetaTests);
-router.get('/:id', Auth.verifyToken, Controller.getBetaTest);
 router.put('/:id', Auth.verifyToken, Controller.updateBetaTest);
+
+/** 운영페이지 내부 요청 및 외부요청(from=external_script) 둘 다 사용 **/
+router.get('/:id', Auth.verify, Controller.getBetaTest);
 
 /* Epilogue */
 router.get('/:betaTestId/epilogue', Auth.verifyToken, EpilogueController.getEpilogue);
