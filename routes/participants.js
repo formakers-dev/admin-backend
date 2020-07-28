8b6f1b20b1d5acbc97dcad9f9b8f1089576ca1f6
Jason <jason.ryu@formakers.net>
2020-07-28 17:13:16 +0900

    feat: 테스트의 특정 참여자에 대한 참여기록 삭제 기능 추가
    https://trello.com/c/r698Gqnh

--- routes/participants.js
+++ routes/participants.js
@@ -6,4 +6,7 @@ const Auth = require('../middlewares/auth');
 router.get('/', Auth.verifyToken, Controller.getParticipants);
 router.post('/', Auth.verifyToken, Controller.registerParticipants);
 router.delete('/:id', Auth.verifyToken, Controller.deleteParticipant);
+
+router.delete('/beta-test/:betaTestId/user/:userId', Auth.verifyToken, Controller.deleteParticipantForBetaTest);
+
 module.exports = router;
